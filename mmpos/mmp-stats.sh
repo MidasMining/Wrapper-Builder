27bd224d4df83c1b3829669d906bb74514ea31e4
Dalkson <26678089+Dalkson@users.noreply.github.com>
2024-07-13 12:27:06 -0400

    Update mmp-stats.sh

--- mmpos/mmp-stats.sh
+++ mmpos/mmp-stats.sh
@@ -4,49 +4,22 @@ LOG_FILE=$2
 cd `dirname $0`
 [ -r mmp-external.conf ] && . mmp-external.conf
 
-get_cpu_hashes() {
-    hash=''
-    local hs=$(cat $LOG_FILE |grep -oP "HASHRATE \K\d+.\d+"|tail -n1)
-    if [[ -z "$hs" ]]; then
-        local hs="0"
-    fi
-    if [[ ${hs} > 0 ]]; then
-        hash=$(echo $hs)
-    fi
-}
-
-get_miner_shares_acc() {
-    acc=''
-    local ac=$(cat $LOG_FILE |grep -oP "ACCEPTED \K\d+"|tail -n1)
-    if [[ -z "$ac" ]]; then
-        local ac="0"
-    fi
-    if [[ ${ac} > 0 ]]; then
-        acc=$(echo $ac)
-    fi
-}
-
-get_miner_shares_rej() {
-    rej=''
-    local rj=$(cat $LOG_FILE |grep -oP "REJECTED \K\d+"|tail -n1)
-    if [[ -z "$rj" ]]; then
-        local rj="0"
-    fi
-    if [[ ${rj} > 0 ]]; then
-        rej=$(echo $rj)
-    fi
-}
-
 get_miner_stats() {
+    stats_raw=`curl -s 'http://localhost:5959/'`
+
+    if [[ $? -ne 0 || -z $stats_raw ]]; then
+        echo -e "${YELLOW}Failed to read $miner from localhost:5959${NOCOLOR}"
+    else
+    
     stats=
     local hash=
-    get_cpu_hashes
+    $(echo $stats_raw | jq -r '.hashrate')
     # A/R shares by pool
     local acc=
-    get_miner_shares_acc
+    $(echo $stats_raw | jq -r '.accepted')
     # local inv=$(get_miner_shares_inv)
     local rej=
-    get_miner_shares_rej
+    REJECTED=$(echo $stats_raw | jq -r '.rejected')
 
     stats=$(jq -nc \
             --argjson hash "$(echo ${hash[@]} | tr " " "\n" | jq -cs '.')" \
